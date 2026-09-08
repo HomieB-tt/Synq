2d558fac2f0f312dfc3798f17402aad531d876d6
Carlton <montanajeremy160@outlook.com>
2026-09-08 23:11:30 +0300

    Added .gitignore.

---
+++ cmd/synq/main.go
@@ -0,0 +1,14 @@
+// Command synq is the entry point for the Synq terminal client.
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	// TODO: initialize crypto key store, db cache, ws client, and the
+	// Bubble Tea root model (internal/app), then run the program.
+	fmt.Fprintln(os.Stderr, "synq: not yet implemented")
+	os.Exit(1)
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/HomieB-tt/synq
+
+go 1.25.0
+
+require golang.org/x/crypto v0.28.0
+
+require golang.org/x/sys v0.47.0 // indirect
